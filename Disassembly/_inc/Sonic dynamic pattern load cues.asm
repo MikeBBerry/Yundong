d34160cf47c2049b028f170d2d240fccf58cbdb1
LuigiXHero <cowdogkid2@yahoo.com>
2016-03-26 19:26:03 -0700

    Setup Git

---
+++ Disassembly/_inc/Sonic dynamic pattern load cues.asm
@@ -0,0 +1,137 @@
+; --------------------------------------------------------------------------------
+; Dynamic Pattern Loading Cues - output from SonMapEd - Sonic 1 format; --------------------------------------------------------------------------------
+
+SME_hJma8:	
+		dc.w SME_hJma8_B0-SME_hJma8, SME_hJma8_B1-SME_hJma8	
+		dc.w SME_hJma8_BA-SME_hJma8, SME_hJma8_C5-SME_hJma8	
+		dc.w SME_hJma8_CC-SME_hJma8, SME_hJma8_D3-SME_hJma8	
+		dc.w SME_hJma8_DA-SME_hJma8, SME_hJma8_E3-SME_hJma8	
+		dc.w SME_hJma8_E8-SME_hJma8, SME_hJma8_ED-SME_hJma8	
+		dc.w SME_hJma8_F6-SME_hJma8, SME_hJma8_FB-SME_hJma8	
+		dc.w SME_hJma8_102-SME_hJma8, SME_hJma8_10D-SME_hJma8	
+		dc.w SME_hJma8_11A-SME_hJma8, SME_hJma8_123-SME_hJma8	
+		dc.w SME_hJma8_12E-SME_hJma8, SME_hJma8_137-SME_hJma8	
+		dc.w SME_hJma8_142-SME_hJma8, SME_hJma8_14B-SME_hJma8	
+		dc.w SME_hJma8_150-SME_hJma8, SME_hJma8_155-SME_hJma8	
+		dc.w SME_hJma8_15E-SME_hJma8, SME_hJma8_163-SME_hJma8	
+		dc.w SME_hJma8_16A-SME_hJma8, SME_hJma8_177-SME_hJma8	
+		dc.w SME_hJma8_184-SME_hJma8, SME_hJma8_18D-SME_hJma8	
+		dc.w SME_hJma8_198-SME_hJma8, SME_hJma8_1A1-SME_hJma8	
+		dc.w SME_hJma8_1AC-SME_hJma8, SME_hJma8_1B1-SME_hJma8	
+		dc.w SME_hJma8_1B6-SME_hJma8, SME_hJma8_1BB-SME_hJma8	
+		dc.w SME_hJma8_1C0-SME_hJma8, SME_hJma8_1C9-SME_hJma8	
+		dc.w SME_hJma8_1D2-SME_hJma8, SME_hJma8_1DF-SME_hJma8	
+		dc.w SME_hJma8_1EC-SME_hJma8, SME_hJma8_1F1-SME_hJma8	
+		dc.w SME_hJma8_1F6-SME_hJma8, SME_hJma8_1FB-SME_hJma8	
+		dc.w SME_hJma8_200-SME_hJma8, SME_hJma8_209-SME_hJma8	
+		dc.w SME_hJma8_210-SME_hJma8, SME_hJma8_219-SME_hJma8	
+		dc.w SME_hJma8_220-SME_hJma8, SME_hJma8_223-SME_hJma8	
+		dc.w SME_hJma8_226-SME_hJma8, SME_hJma8_229-SME_hJma8	
+		dc.w SME_hJma8_22C-SME_hJma8, SME_hJma8_22F-SME_hJma8	
+		dc.w SME_hJma8_234-SME_hJma8, SME_hJma8_237-SME_hJma8	
+		dc.w SME_hJma8_23C-SME_hJma8, SME_hJma8_23F-SME_hJma8	
+		dc.w SME_hJma8_244-SME_hJma8, SME_hJma8_24D-SME_hJma8	
+		dc.w SME_hJma8_256-SME_hJma8, SME_hJma8_25D-SME_hJma8	
+		dc.w SME_hJma8_264-SME_hJma8, SME_hJma8_26B-SME_hJma8	
+		dc.w SME_hJma8_26E-SME_hJma8, SME_hJma8_275-SME_hJma8	
+		dc.w SME_hJma8_276-SME_hJma8, SME_hJma8_27D-SME_hJma8	
+		dc.w SME_hJma8_286-SME_hJma8, SME_hJma8_28F-SME_hJma8	
+		dc.w SME_hJma8_29A-SME_hJma8, SME_hJma8_2A5-SME_hJma8	
+		dc.w SME_hJma8_2AA-SME_hJma8, SME_hJma8_2B1-SME_hJma8	
+		dc.w SME_hJma8_2B6-SME_hJma8, SME_hJma8_2BD-SME_hJma8	
+		dc.w SME_hJma8_2C2-SME_hJma8, SME_hJma8_2C3-SME_hJma8	
+		dc.w SME_hJma8_2C4-SME_hJma8, SME_hJma8_2CF-SME_hJma8	
+		dc.w SME_hJma8_2DA-SME_hJma8, SME_hJma8_2DB-SME_hJma8	
+		dc.w SME_hJma8_2DC-SME_hJma8, SME_hJma8_2DD-SME_hJma8	
+		dc.w SME_hJma8_2DE-SME_hJma8, SME_hJma8_2DF-SME_hJma8	
+		dc.w SME_hJma8_2E6-SME_hJma8, SME_hJma8_2ED-SME_hJma8	
+		dc.w SME_hJma8_2F4-SME_hJma8, SME_hJma8_2FB-SME_hJma8	
+SME_hJma8_B0:	dc.b 0	
+SME_hJma8_B1:	dc.b 4, $20, 0, $70, 3, $20, $B, $20, $E	
+SME_hJma8_BA:	dc.b 5, $20, $1D, $50, $11, $30, $17, 0, $1C, 4, $60	
+SME_hJma8_C5:	dc.b 3, $50, $20, $50, $17, $20, $1D	
+SME_hJma8_CC:	dc.b 3, $50, $20, $50, $17, $20, $26	
+SME_hJma8_D3:	dc.b 3, $80, $29, $20, $B, $20, $E	
+SME_hJma8_DA:	dc.b 4, $70, $32, $50, $3A, $50, $40, $10, $46	
+SME_hJma8_E3:	dc.b 2, $70, $32, $B0, $48	
+SME_hJma8_E8:	dc.b 2, $50, $54, $80, $5A	
+SME_hJma8_ED:	dc.b 4, $50, $54, $50, $63, $50, $69, $10, $6F	
+SME_hJma8_F6:	dc.b 2, $50, $54, $B0, $71	
+SME_hJma8_FB:	dc.b 3, $70, $32, $30, $7D, $50, $81	
+SME_hJma8_102:	dc.b 5, $50, $8D, $20, $93, $50, $96, 0, $9C, $53, $AA	
+SME_hJma8_10D:	dc.b 6, $50, $87, $10, $9D, $30, $9F, $50, $A3, $30, $A9, 0, $AD	
+SME_hJma8_11A:	dc.b 4, $50, $AE, $10, $B4, $70, $B6, $20, $BE	
+SME_hJma8_123:	dc.b 5, $30, $C7, $70, $CB, $20, $D3, $10, $D6, $53, $B0	
+SME_hJma8_12E:	dc.b 4, $50, $C1, $10, $D8, $70, $DA, $20, $E2	
+SME_hJma8_137:	dc.b 5, $50, $87, $10, $9D, 0, $93, $70, $E5, $20, $ED	
+SME_hJma8_142:	dc.b 4, $70, $F0, $50, $F8, $10, $FE, $51, 0	
+SME_hJma8_14B:	dc.b 2, $70, $F0, $B1, 6	
+SME_hJma8_150:	dc.b 2, $51, $12, $81, $18	
+SME_hJma8_155:	dc.b 4, $51, $12, $51, $21, $11, $27, $51, $29	
+SME_hJma8_15E:	dc.b 2, $51, $12, $B1, $2F	
+SME_hJma8_163:	dc.b 3, $70, $F0, 1, 6, $81, $3B	
+SME_hJma8_16A:	dc.b 6, $11, $4A, $11, $4C, $81, $4E, 1, $57, 1, $58, $54, $61	
+SME_hJma8_177:	dc.b 6, $51, $44, $21, $59, $11, $5C, $11, $5E, $81, $60, 1, $57	
+SME_hJma8_184:	dc.b 4, $51, $69, $11, $6F, $81, $71, $11, $7A	
+SME_hJma8_18D:	dc.b 5, $21, $82, $11, $85, $71, $87, $21, $8F, $54, $6D	
+SME_hJma8_198:	dc.b 4, $51, $7C, $11, $92, $81, $94, $11, $9D	
+SME_hJma8_1A1:	dc.b 5, $51, $44, $81, $9F, $11, $5E, $11, $A8, 4, $75	
+SME_hJma8_1AC:	dc.b 2, $51, $AA, $B1, $B0	
+SME_hJma8_1B1:	dc.b 2, $50, $54, $B1, $BC	
+SME_hJma8_1B6:	dc.b 2, $51, $AA, $B1, $C8	
+SME_hJma8_1BB:	dc.b 2, $50, $54, $B1, $D4	
+SME_hJma8_1C0:	dc.b 4, $51, $E0, $11, $E6, $B1, $E8, 1, $F4	
+SME_hJma8_1C9:	dc.b 4, $51, $F5, $11, $FB, $B1, $FD, 3, $B6	
+SME_hJma8_1D2:	dc.b 6, $12, 9, $B2, $B, $14, $67, 4, $69, $14, $6A, $14, $9D	
+SME_hJma8_1DF:	dc.b 6, $B2, $17, $13, $B7, $14, $73, $14, $99, $14, $9B, 4, $9F	
+SME_hJma8_1EC:	dc.b 2, $52, $23, $B2, $29	
+SME_hJma8_1F1:	dc.b 2, $51, $12, $B2, $35	
+SME_hJma8_1F6:	dc.b 2, $52, $23, $B2, $41	
+SME_hJma8_1FB:	dc.b 2, $51, $12, $B2, $4D	
+SME_hJma8_200:	dc.b 4, $52, $59, $12, $5F, $B2, $61, 2, $6D	
+SME_hJma8_209:	dc.b 3, $72, $6E, $B2, $76, 4, $AF	
+SME_hJma8_210:	dc.b 4, $12, $82, $B2, $84, 2, $6D, $54, $A9	
+SME_hJma8_219:	dc.b 3, $B2, $90, 4, $A0, $74, $A1	
+SME_hJma8_220:	dc.b 1, $F2, $9C	
+SME_hJma8_223:	dc.b 1, $F2, $AC	
+SME_hJma8_226:	dc.b 1, $F2, $BC	
+SME_hJma8_229:	dc.b 1, $F2, $CC	
+SME_hJma8_22C:	dc.b 1, $F2, $DC	
+SME_hJma8_22F:	dc.b 2, $B2, $EC, $22, $F8	
+SME_hJma8_234:	dc.b 1, $F2, $FB	
+SME_hJma8_237:	dc.b 2, $B3, $B, $23, $17	
+SME_hJma8_23C:	dc.b 1, $F3, $1A	
+SME_hJma8_23F:	dc.b 2, $53, $2A, $B3, $30	
+SME_hJma8_244:	dc.b 4, $53, $3C, $73, $42, $13, $4A, 3, $4C	
+SME_hJma8_24D:	dc.b 4, $13, $4D, $73, $4F, $23, $57, 3, $5A	
+SME_hJma8_256:	dc.b 3, $23, $5B, $23, $5E, $F3, $61	
+SME_hJma8_25D:	dc.b 3, $B3, $71, $73, $7D, 0, $71	
+SME_hJma8_264:	dc.b 3, $73, $85, $33, $8D, $23, $91	
+SME_hJma8_26B:	dc.b 1, $83, $94	
+SME_hJma8_26E:	dc.b 3, $73, $9D, 3, $A5, $33, $A6	
+SME_hJma8_275:	dc.b 0	
+SME_hJma8_276:	dc.b 3, $B3, $B9, $13, $C5, 3, $C7	
+SME_hJma8_27D:	dc.b 4, $B3, $C8, $33, $D4, 3, $D8, 3, $D9	
+SME_hJma8_286:	dc.b 4, $B3, $DA, $33, $E6, 3, $EA, 3, $EB	
+SME_hJma8_28F:	dc.b 5, $83, $EC, $13, $F5, $53, $F7, $13, $FD, 3, $FF	
+SME_hJma8_29A:	dc.b 5, $84, 0, $14, 9, $53, $F7, $13, $FD, 3, $FF	
+SME_hJma8_2A5:	dc.b 2, $84, $B, $74, $14	
+SME_hJma8_2AA:	dc.b 3, $84, $1C, $24, $25, $14, $28	
+SME_hJma8_2B1:	dc.b 2, $84, $2A, $74, $33	
+SME_hJma8_2B6:	dc.b 3, $84, $1C, $24, $3B, $14, $3E	
+SME_hJma8_2BD:	dc.b 2, $54, $40, $B4, $46	
+SME_hJma8_2C2:	dc.b 0	
+SME_hJma8_2C3:	dc.b 0	
+SME_hJma8_2C4:	dc.b 5, $74, $76, $14, $7E, $54, $80, $34, $86, 4, $8A	
+SME_hJma8_2CF:	dc.b 5, $74, $8B, $14, $7E, $54, $93, $34, $86, 4, $8A	
+SME_hJma8_2DA:	dc.b 0	
+SME_hJma8_2DB:	dc.b 0	
+SME_hJma8_2DC:	dc.b 0	
+SME_hJma8_2DD:	dc.b 0	
+SME_hJma8_2DE:	dc.b 0	
+SME_hJma8_2DF:	dc.b 3, $73, $85, $33, $8D, $23, $91	
+SME_hJma8_2E6:	dc.b 3, $73, $9D, 3, $A5, $33, $A6	
+SME_hJma8_2ED:	dc.b 3, $B4, $D2, $14, $DE, $34, $E0	
+SME_hJma8_2F4:	dc.b 3, $54, $E4, $B4, $EA, $10, $6D	
+SME_hJma8_2FB:	dc.b 2, $F4, $F6, $25, 6	
+		even
---
+++ Disassembly/_maps/Sonic.asm
@@ -0,0 +1,391 @@
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME_jLMae:	
+		dc.w SME_jLMae_B0-SME_jLMae, SME_jLMae_B1-SME_jLMae	
+		dc.w SME_jLMae_C6-SME_jLMae, SME_jLMae_E0-SME_jLMae	
+		dc.w SME_jLMae_F0-SME_jLMae, SME_jLMae_100-SME_jLMae	
+		dc.w SME_jLMae_110-SME_jLMae, SME_jLMae_125-SME_jLMae	
+		dc.w SME_jLMae_130-SME_jLMae, SME_jLMae_13B-SME_jLMae	
+		dc.w SME_jLMae_150-SME_jLMae, SME_jLMae_15B-SME_jLMae	
+		dc.w SME_jLMae_16B-SME_jLMae, SME_jLMae_185-SME_jLMae	
+		dc.w SME_jLMae_1A4-SME_jLMae, SME_jLMae_1B9-SME_jLMae	
+		dc.w SME_jLMae_1D3-SME_jLMae, SME_jLMae_1E8-SME_jLMae	
+		dc.w SME_jLMae_202-SME_jLMae, SME_jLMae_217-SME_jLMae	
+		dc.w SME_jLMae_222-SME_jLMae, SME_jLMae_22D-SME_jLMae	
+		dc.w SME_jLMae_242-SME_jLMae, SME_jLMae_24D-SME_jLMae	
+		dc.w SME_jLMae_25D-SME_jLMae, SME_jLMae_27C-SME_jLMae	
+		dc.w SME_jLMae_29B-SME_jLMae, SME_jLMae_2B0-SME_jLMae	
+		dc.w SME_jLMae_2CA-SME_jLMae, SME_jLMae_2DF-SME_jLMae	
+		dc.w SME_jLMae_2F9-SME_jLMae, SME_jLMae_304-SME_jLMae	
+		dc.w SME_jLMae_30F-SME_jLMae, SME_jLMae_31A-SME_jLMae	
+		dc.w SME_jLMae_325-SME_jLMae, SME_jLMae_33A-SME_jLMae	
+		dc.w SME_jLMae_34F-SME_jLMae, SME_jLMae_36E-SME_jLMae	
+		dc.w SME_jLMae_38D-SME_jLMae, SME_jLMae_398-SME_jLMae	
+		dc.w SME_jLMae_3A3-SME_jLMae, SME_jLMae_3AE-SME_jLMae	
+		dc.w SME_jLMae_3B9-SME_jLMae, SME_jLMae_3CE-SME_jLMae	
+		dc.w SME_jLMae_3DE-SME_jLMae, SME_jLMae_3F3-SME_jLMae	
+		dc.w SME_jLMae_403-SME_jLMae, SME_jLMae_409-SME_jLMae	
+		dc.w SME_jLMae_40F-SME_jLMae, SME_jLMae_415-SME_jLMae	
+		dc.w SME_jLMae_41B-SME_jLMae, SME_jLMae_421-SME_jLMae	
+		dc.w SME_jLMae_42C-SME_jLMae, SME_jLMae_432-SME_jLMae	
+		dc.w SME_jLMae_43D-SME_jLMae, SME_jLMae_443-SME_jLMae	
+		dc.w SME_jLMae_44E-SME_jLMae, SME_jLMae_463-SME_jLMae	
+		dc.w SME_jLMae_478-SME_jLMae, SME_jLMae_488-SME_jLMae	
+		dc.w SME_jLMae_498-SME_jLMae, SME_jLMae_4A8-SME_jLMae	
+		dc.w SME_jLMae_4B3-SME_jLMae, SME_jLMae_4C3-SME_jLMae	
+		dc.w SME_jLMae_4C4-SME_jLMae, SME_jLMae_4D4-SME_jLMae	
+		dc.w SME_jLMae_4E9-SME_jLMae, SME_jLMae_4FE-SME_jLMae	
+		dc.w SME_jLMae_518-SME_jLMae, SME_jLMae_532-SME_jLMae	
+		dc.w SME_jLMae_53D-SME_jLMae, SME_jLMae_54D-SME_jLMae	
+		dc.w SME_jLMae_558-SME_jLMae, SME_jLMae_568-SME_jLMae	
+		dc.w SME_jLMae_573-SME_jLMae, SME_jLMae_574-SME_jLMae	
+		dc.w SME_jLMae_575-SME_jLMae, SME_jLMae_58F-SME_jLMae	
+		dc.w SME_jLMae_5A9-SME_jLMae, SME_jLMae_5AA-SME_jLMae	
+		dc.w SME_jLMae_5AB-SME_jLMae, SME_jLMae_5AC-SME_jLMae	
+		dc.w SME_jLMae_5AD-SME_jLMae, SME_jLMae_5AE-SME_jLMae	
+		dc.w SME_jLMae_5BE-SME_jLMae, SME_jLMae_5CE-SME_jLMae	
+		dc.w SME_jLMae_5DE-SME_jLMae, SME_jLMae_5EE-SME_jLMae	
+SME_jLMae_B0:	dc.b 0	
+SME_jLMae_B1:	dc.b 4	
+		dc.b $EC, 8, 0, 0, $F0	
+		dc.b $F4, $D, 0, 3, $F0	
+		dc.b 4, 8, 0, $B, $F0	
+		dc.b $C, 8, 0, $E, $F8	
+SME_jLMae_C6:	dc.b 5	
+		dc.b $C, 8, 0, 0, $F8	
+		dc.b $EC, 9, 0, 3, $F0	
+		dc.b $FC, 5, 0, 9, $F0	
+		dc.b 4, 0, 0, $D, 0	
+		dc.b $FC, 0, 0, $E, 0	
+SME_jLMae_E0:	dc.b 3	
+		dc.b $EC, 9, 0, 0, $F0	
+		dc.b $FC, 9, 0, 6, $F0	
+		dc.b $C, 8, 0, $C, $F8	
+SME_jLMae_F0:	dc.b 3	
+		dc.b $EC, 9, 0, 0, $F0	
+		dc.b $FC, 9, 0, 6, $F0	
+		dc.b $C, 8, 0, $C, $F8	
+SME_jLMae_100:	dc.b 3	
+		dc.b $EC, $A, 0, 0, $F0	
+		dc.b 4, 8, 0, 9, $F0	
+		dc.b $C, 8, 0, $C, $F8	
+SME_jLMae_110:	dc.b 4	
+		dc.b $EB, $D, 0, 0, $EC	
+		dc.b $FB, 9, 0, 8, $EC	
+		dc.b $FB, 6, 0, $E, 4	
+		dc.b $B, 4, 0, $14, $EC	
+SME_jLMae_125:	dc.b 2	
+		dc.b $EC, $D, 0, 0, $ED	
+		dc.b $FC, $E, 0, 8, $F5	
+SME_jLMae_130:	dc.b 2	
+		dc.b $ED, 9, 0, 0, $F3	
+		dc.b $FD, $A, 0, 6, $F3	
+SME_jLMae_13B:	dc.b 4	
+		dc.b $EB, 9, 0, 0, $F3	
+		dc.b $FB, 9, 0, 6, $EC	
+		dc.b $FB, 6, 0, $C, 4	
+		dc.b $B, 4, 0, $12, $EC	
+SME_jLMae_150:	dc.b 2	
+		dc.b $EC, 9, 0, 0, $F2	
+		dc.b $FC, $E, 0, 6, $EB	
+SME_jLMae_15B:	dc.b 3	
+		dc.b $ED, $D, 0, 0, $EC	
+		dc.b $FD, $C, 0, 8, $F4	
+		dc.b 5, 9, 0, $C, $F4	
+SME_jLMae_16B:	dc.b 5	
+		dc.b $EB, 6, 0, 0, 3	
+		dc.b $FB, 8, 0, 6, $EB	
+		dc.b 3, 9, 0, 9, $F3	
+		dc.b $13, 0, 0, $F, $FB	
+		dc.b $EB, 9, 0, $10, $EB	
+SME_jLMae_185:	dc.b 6	
+		dc.b $EC, 9, 0, 0, $EC	
+		dc.b $EC, 1, 0, 6, 4	
+		dc.b $FC, $C, 0, 8, $EC	
+		dc.b 4, 9, 0, $C, $F4	
+		dc.b $FC, 5, 0, $12, $C	
+		dc.b $F4, 0, 0, $16, $14	
+SME_jLMae_1A4:	dc.b 4	
+		dc.b $ED, 9, 0, 0, $ED	
+		dc.b $ED, 1, 0, 6, 5	
+		dc.b $FD, $D, 0, 8, $F5	
+		dc.b $D, 8, 0, $10, $FD	
+SME_jLMae_1B9:	dc.b 5	
+		dc.b $EB, 5, 0, 0, 3	
+		dc.b $FB, $D, 0, 4, $F3	
+		dc.b $B, 8, 0, $C, $F3	
+		dc.b $13, 4, 0, $F, $FB	
+		dc.b $EB, 9, 0, $11, $EB	
+SME_jLMae_1D3:	dc.b 4	
+		dc.b $EC, 9, 0, 0, $EC	
+		dc.b $EC, 1, 0, 6, 4	
+		dc.b $FC, $D, 0, 8, $F4	
+		dc.b $C, 8, 0, $10, $FC	
+SME_jLMae_1E8:	dc.b 5	
+		dc.b $ED, 9, 0, 0, $ED	
+		dc.b $ED, 1, 0, 6, 5	
+		dc.b $FD, 0, 0, 8, $ED	
+		dc.b $FD, $D, 0, 9, $F5	
+		dc.b $D, 8, 0, $11, $FD	
+SME_jLMae_202:	dc.b 4	
+		dc.b $F4, 7, 0, 0, $EB	
+		dc.b $EC, 9, 0, 8, $FB	
+		dc.b $FC, 4, 0, $E, $FB	
+		dc.b 4, 9, 0, $10, $FB	
+SME_jLMae_217:	dc.b 2	
+		dc.b $F4, 7, 0, 0, $EC	
+		dc.b $EC, $B, 0, 8, $FC	
+SME_jLMae_222:	dc.b 2	
+		dc.b $F4, 6, 0, 0, $ED	
+		dc.b $F4, $A, 0, 6, $FD	
+SME_jLMae_22D:	dc.b 4	
+		dc.b $F5, 6, 0, 0, $EB	
+		dc.b $EC, 9, 0, 6, $FB	
+		dc.b $FC, 4, 0, $C, $FB	
+		dc.b 4, 9, 0, $E, $FB	
+SME_jLMae_242:	dc.b 2	
+		dc.b $F5, 6, 0, 0, $EC	
+		dc.b $F4, $B, 0, 6, $FC	
+SME_jLMae_24D:	dc.b 3	
+		dc.b $F4, 7, 0, 0, $ED	
+		dc.b $EC, 0, 0, 8, $FD	
+		dc.b $F4, $A, 0, 9, $FD	
+SME_jLMae_25D:	dc.b 6	
+		dc.b $ED, 4, 0, 0, $F3	
+		dc.b $F5, 4, 0, 2, $EB	
+		dc.b $F5, $A, 0, 4, $FB	
+		dc.b $D, 0, 0, $D, $FB	
+		dc.b $FD, 0, 0, $E, $13	
+		dc.b $FD, 6, 0, $F, $EB	
+SME_jLMae_27C:	dc.b 6	
+		dc.b $FC, 6, 0, 0, $EC	
+		dc.b $E4, 8, 0, 6, $F4	
+		dc.b $EC, 4, 0, 9, $FC	
+		dc.b $F4, 4, 0, $B, $EC	
+		dc.b $F4, $A, 0, $D, $FC	
+		dc.b $C, 0, 0, $16, $FC	
+SME_jLMae_29B:	dc.b 4	
+		dc.b $FB, 6, 0, 0, $ED	
+		dc.b $F3, 4, 0, 6, $ED	
+		dc.b $EB, $A, 0, 8, $FD	
+		dc.b 3, 4, 0, $11, $FD	
+SME_jLMae_2B0:	dc.b 5	
+		dc.b $ED, 8, 0, 0, $F3	
+		dc.b $F5, 4, 0, 3, $EB	
+		dc.b $F5, $D, 0, 5, $FB	
+		dc.b 5, 8, 0, $D, $FB	
+		dc.b $FD, 6, 0, $10, $EB	
+SME_jLMae_2CA:	dc.b 4	
+		dc.b $FC, 6, 0, 0, $EC	
+		dc.b $F4, 4, 0, 6, $EC	
+		dc.b $EC, $A, 0, 8, $FC	
+		dc.b 4, 4, 0, $11, $FC	
+SME_jLMae_2DF:	dc.b 5	
+		dc.b $FB, 6, 0, 0, $ED	
+		dc.b $EB, $A, 0, 6, $FD	
+		dc.b $F3, 4, 0, $F, $ED	
+		dc.b 3, 4, 0, $11, $FD	
+		dc.b $B, 0, 0, $13, $FD	
+SME_jLMae_2F9:	dc.b 2	
+		dc.b $EE, 9, 0, 0, $F4	
+		dc.b $FE, $E, 0, 6, $EC	
+SME_jLMae_304:	dc.b 2	
+		dc.b $EE, 9, 0, 0, $F4	
+		dc.b $FE, $E, 0, 6, $EC	
+SME_jLMae_30F:	dc.b 2	
+		dc.b $EE, 9, 0, 0, $F4	
+		dc.b $FE, $E, 0, 6, $ED	
+SME_jLMae_31A:	dc.b 2	
+		dc.b $EE, 9, 0, 0, $F4	
+		dc.b $FE, $E, 0, 6, $EC	
+SME_jLMae_325:	dc.b 4	
+		dc.b $EE, 9, 0, 0, $EE	
+		dc.b $EE, 1, 0, 6, 6	
+		dc.b $FE, $E, 0, 8, $F6	
+		dc.b $FE, 0, 0, $14, $EE	
+SME_jLMae_33A:	dc.b 4	
+		dc.b $EE, 9, 0, 0, $EE	
+		dc.b $EE, 1, 0, 6, 6	
+		dc.b $FE, $E, 0, 8, $F6	
+		dc.b $FE, 0, 0, $14, $EE	
+SME_jLMae_34F:	dc.b 6	
+		dc.b $EE, 1, 0, 0, 6	
+		dc.b $FE, $E, 0, 2, $F6	
+		dc.b $EE, 1, 0, $E, $FE	
+		dc.b $FE, 0, 0, $10, $EE	
+		dc.b $EE, 1, 0, $11, $F6	
+		dc.b $EE, 1, 0, $13, $EE	
+SME_jLMae_36E:	dc.b 6	
+		dc.b $FE, $E, 0, 0, $F6	
+		dc.b $F6, 4, 0, $C, $F6	
+		dc.b $EE, 4, 0, $E, $F6	
+		dc.b $EE, 1, 0, $10, $EE	
+		dc.b $EE, 1, 0, $12, 6	
+		dc.b $FE, 0, 0, $14, $EE	
+SME_jLMae_38D:	dc.b 2	
+		dc.b $F4, 6, 0, 0, $EE	
+		dc.b $F4, $B, 0, 6, $FE	
+SME_jLMae_398:	dc.b 2	
+		dc.b $F4, 6, 0, 0, $EE	
+		dc.b $F4, $B, 0, 6, $FE	
+SME_jLMae_3A3:	dc.b 2	
+		dc.b $F4, 6, 0, 0, $EE	
+		dc.b $F4, $B, 0, 6, $FE	
+SME_jLMae_3AE:	dc.b 2	
+		dc.b $F4, 6, 0, 0, $EE	
+		dc.b $F4, $B, 0, 6, $FE	
+SME_jLMae_3B9:	dc.b 4	
+		dc.b $FA, 6, 0, 0, $EE	
+		dc.b $F2, 4, 0, 6, $EE	
+		dc.b $EA, $B, 0, 8, $FE	
+		dc.b $A, 0, 0, $14, $FE	
+SME_jLMae_3CE:	dc.b 3	
+		dc.b $F2, 7, 0, 0, $EE	
+		dc.b $EA, $B, 0, 8, $FE	
+		dc.b $A, 0, 0, $14, $FE	
+SME_jLMae_3DE:	dc.b 4	
+		dc.b $F2, 4, 0, 0, $EE	
+		dc.b $EA, $B, 0, 2, $FE	
+		dc.b $A, 0, 0, $E, $FE	
+		dc.b $FA, 6, 0, $F, $EE	
+SME_jLMae_3F3:	dc.b 3	
+		dc.b $EA, $B, 0, 0, $FE	
+		dc.b $A, 0, 0, $C, $FE	
+		dc.b $F2, 7, 0, $D, $EE	
+SME_jLMae_403:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_jLMae_409:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_jLMae_40F:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_jLMae_415:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_jLMae_41B:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_jLMae_421:	dc.b 2	
+		dc.b $F4, $E, 0, 0, $EC	
+		dc.b $F4, 2, 0, $C, $C	
+SME_jLMae_42C:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_jLMae_432:	dc.b 2	
+		dc.b $EC, $B, 0, 0, $F4	
+		dc.b $C, 8, 0, $C, $F4	
+SME_jLMae_43D:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_jLMae_443:	dc.b 2	
+		dc.b $ED, 9, 0, 0, $F0	
+		dc.b $FD, $E, 0, 6, $F0	
+SME_jLMae_44E:	dc.b 4	
+		dc.b $ED, 9, 0, 0, $F0	
+		dc.b $FD, $D, 0, 6, $F0	
+		dc.b $D, 4, 0, $E, 0	
+		dc.b 5, 0, 0, $10, $E8	
+SME_jLMae_463:	dc.b 4	
+		dc.b $F4, 4, 0, 0, $FC	
+		dc.b $FC, $D, 0, 2, $F4	
+		dc.b $C, 8, 0, $A, $F4	
+		dc.b 4, 0, 0, $D, $EC	
+SME_jLMae_478:	dc.b 3	
+		dc.b $EC, 8, 8, 0, $E0	
+		dc.b $F4, 2, 8, 3, $F8	
+		dc.b $F4, $F, 8, 6, $D8	
+SME_jLMae_488:	dc.b 3	
+		dc.b $EC, $E, 8, 0, $E0	
+		dc.b 4, $D, 8, $C, $D8	
+		dc.b $C, 0, $18, $14, $F8	
+SME_jLMae_498:	dc.b 3	
+		dc.b $F4, $D, 0, 0, $FC	
+		dc.b $FC, 5, 0, 8, $EC	
+		dc.b 4, 8, 0, $C, $FC	
+SME_jLMae_4A8:	dc.b 2	
+		dc.b $F4, $A, 0, 0, $E8	
+		dc.b $F4, $A, 8, 0, 0	
+SME_jLMae_4B3:	dc.b 3	
+		dc.b $F4, $D, 0, 0, $E4	
+		dc.b $FC, 0, 0, 8, 4	
+		dc.b 4, $C, 0, 9, $EC	
+SME_jLMae_4C3:	dc.b 0	
+SME_jLMae_4C4:	dc.b 3	
+		dc.b $E8, $B, 0, 0, $F0	
+		dc.b 8, 4, 0, $C, $F8	
+		dc.b $10, 0, 0, $E, $F8	
+SME_jLMae_4D4:	dc.b 4	
+		dc.b $F8, $E, 0, 0, $E8	
+		dc.b 0, 5, 0, $C, 8	
+		dc.b $F8, 0, 0, $10, 8	
+		dc.b $F0, 0, 0, $11, $F8	
+SME_jLMae_4E9:	dc.b 4	
+		dc.b $F8, $E, 0, 0, $E8	
+		dc.b 0, 5, 0, $C, 8	
+		dc.b $F8, 0, 0, $10, 8	
+		dc.b $F0, 0, 0, $11, $F8	
+SME_jLMae_4FE:	dc.b 5	
+		dc.b $E8, $A, 0, 0, $F4	
+		dc.b $F0, 1, 0, 9, $C	
+		dc.b 0, 9, 0, $B, $F4	
+		dc.b $10, 4, 0, $11, $F4	
+		dc.b 0, 0, 0, $13, $EC	
+SME_jLMae_518:	dc.b 5	
+		dc.b $E8, $A, 0, 0, $F4	
+		dc.b $E8, 1, 0, 9, $C	
+		dc.b 0, 9, 0, $B, $F4	
+		dc.b $10, 4, 0, $11, $F4	
+		dc.b 0, 0, 0, $13, $EC	
+SME_jLMae_532:	dc.b 2	
+		dc.b $ED, $A, 0, 0, $F3	
+		dc.b 5, $D, 0, 9, $EB	
+SME_jLMae_53D:	dc.b 3	
+		dc.b $EC, $A, 0, 0, $F3	
+		dc.b 4, 8, 0, 9, $F3	
+		dc.b $C, 4, 0, $C, $F3	
+SME_jLMae_54D:	dc.b 2	
+		dc.b $ED, $A, 0, 0, $F3	
+		dc.b 5, $D, 0, 9, $EB	
+SME_jLMae_558:	dc.b 3	
+		dc.b $EC, $A, 0, 0, $F3	
+		dc.b 4, 8, 0, 9, $F3	
+		dc.b $C, 4, 0, $C, $F3	
+SME_jLMae_568:	dc.b 2	
+		dc.b $EC, 9, 0, 0, $F0	
+		dc.b $FC, $E, 0, 6, $F0	
+SME_jLMae_573:	dc.b 0	
+SME_jLMae_574:	dc.b 0	
+SME_jLMae_575:	dc.b 5	
+		dc.b $E8, $D, 0, 0, $EC	
+		dc.b $E8, 1, 0, 8, $C	
+		dc.b $F8, 9, 0, $A, $F4	
+		dc.b 8, $C, 0, $10, $F4	
+		dc.b $10, 0, 0, $14, $F4	
+SME_jLMae_58F:	dc.b 5	
+		dc.b $E8, $D, 0, 0, $EC	
+		dc.b $E8, 1, 0, 8, $C	
+		dc.b $F8, 9, 0, $A, $F4	
+		dc.b 8, $C, 0, $10, $F4	
+		dc.b $10, 0, 0, $14, $F4	
+SME_jLMae_5A9:	dc.b 0	
+SME_jLMae_5AA:	dc.b 0	
+SME_jLMae_5AB:	dc.b 0	
+SME_jLMae_5AC:	dc.b 0	
+SME_jLMae_5AD:	dc.b 0	
+SME_jLMae_5AE:	dc.b 3	
+		dc.b $F4, $D, 8, 0, $E4	
+		dc.b $FC, 5, 8, 8, 4	
+		dc.b 4, 8, 8, $C, $EC	
+SME_jLMae_5BE:	dc.b 3	
+		dc.b $F4, $D, 8, 0, $FC	
+		dc.b $FC, 0, 8, 8, $F4	
+		dc.b 4, $C, 8, 9, $F4	
+SME_jLMae_5CE:	dc.b 3	
+		dc.b $F0, $E, 0, 0, $EC	
+		dc.b $F8, 1, 0, $C, $C	
+		dc.b 8, $C, 0, $E, $F4	
+SME_jLMae_5DE:	dc.b 3	
+		dc.b $EB, 9, 0, 0, $F4	
+		dc.b $FB, $E, 0, 6, $EC	
+		dc.b 3, 1, 0, $12, $C	
+SME_jLMae_5EE:	dc.b 2	
+		dc.b $F0, $F, 0, 0, $EC	
+		dc.b $F8, 2, 0, $10, $C	
+		even
