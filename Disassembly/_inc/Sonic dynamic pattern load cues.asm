1dfdc394c741d0f56e1321ddaaf8e5898fbe5350
LuigiXHero <cowdogkid2@yahoo.com>
2016-04-02 19:04:18 -0700

    Fixed Balanceing animation.

--- Disassembly/_inc/Sonic dynamic pattern load cues.asm
+++ Disassembly/_inc/Sonic dynamic pattern load cues.asm
@@ -1,116 +1,116 @@
 ; --------------------------------------------------------------------------------
 ; Dynamic Pattern Loading Cues - output from SonMapEd - Sonic 1 format; --------------------------------------------------------------------------------
 
-SME_Z3Iyr:	
-		dc.w SME_Z3Iyr_94-SME_Z3Iyr, SME_Z3Iyr_95-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_9E-SME_Z3Iyr, SME_Z3Iyr_AD-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_B4-SME_Z3Iyr, SME_Z3Iyr_BB-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_C2-SME_Z3Iyr, SME_Z3Iyr_CB-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_D0-SME_Z3Iyr, SME_Z3Iyr_D5-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_DE-SME_Z3Iyr, SME_Z3Iyr_E3-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_EA-SME_Z3Iyr, SME_Z3Iyr_F5-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_102-SME_Z3Iyr, SME_Z3Iyr_10B-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_116-SME_Z3Iyr, SME_Z3Iyr_11F-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_12A-SME_Z3Iyr, SME_Z3Iyr_133-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_138-SME_Z3Iyr, SME_Z3Iyr_13D-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_146-SME_Z3Iyr, SME_Z3Iyr_14B-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_152-SME_Z3Iyr, SME_Z3Iyr_15F-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_16C-SME_Z3Iyr, SME_Z3Iyr_175-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_180-SME_Z3Iyr, SME_Z3Iyr_189-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_194-SME_Z3Iyr, SME_Z3Iyr_199-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_19E-SME_Z3Iyr, SME_Z3Iyr_1A3-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_1A8-SME_Z3Iyr, SME_Z3Iyr_1B1-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_1BA-SME_Z3Iyr, SME_Z3Iyr_1C7-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_1D4-SME_Z3Iyr, SME_Z3Iyr_1D9-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_1DE-SME_Z3Iyr, SME_Z3Iyr_1E3-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_1E8-SME_Z3Iyr, SME_Z3Iyr_1F1-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_1F8-SME_Z3Iyr, SME_Z3Iyr_201-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_208-SME_Z3Iyr, SME_Z3Iyr_20D-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_216-SME_Z3Iyr, SME_Z3Iyr_21D-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_222-SME_Z3Iyr, SME_Z3Iyr_22B-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_234-SME_Z3Iyr, SME_Z3Iyr_23B-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_242-SME_Z3Iyr, SME_Z3Iyr_249-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_24C-SME_Z3Iyr, SME_Z3Iyr_253-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_25A-SME_Z3Iyr, SME_Z3Iyr_263-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_26C-SME_Z3Iyr, SME_Z3Iyr_271-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_278-SME_Z3Iyr, SME_Z3Iyr_27D-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_284-SME_Z3Iyr, SME_Z3Iyr_28F-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_29A-SME_Z3Iyr, SME_Z3Iyr_2A1-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_2A8-SME_Z3Iyr, SME_Z3Iyr_2AF-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_2B4-SME_Z3Iyr, SME_Z3Iyr_2BB-SME_Z3Iyr	
-		dc.w SME_Z3Iyr_2C4-SME_Z3Iyr, SME_Z3Iyr_2D5-SME_Z3Iyr	
-SME_Z3Iyr_94:	dc.b 0	
-SME_Z3Iyr_95:	dc.b 4, $20, 0, $20, 9, $20, $C, $50, 3	
-SME_Z3Iyr_9E:	dc.b 7, $20, $1B, $50, $F, 0, $1A, 3, $91, $10, $15, 0, $18, 4, $81	
-SME_Z3Iyr_AD:	dc.b 3, $50, $1E, $50, $15, $20, $1B	
-SME_Z3Iyr_B4:	dc.b 3, $50, $1E, $50, $15, $20, $24	
-SME_Z3Iyr_BB:	dc.b 3, $80, $27, $20, 9, $20, $C	
-SME_Z3Iyr_C2:	dc.b 4, $70, $30, $50, $38, $50, $3E, $10, $44	
-SME_Z3Iyr_CB:	dc.b 2, $70, $30, $B0, $46	
-SME_Z3Iyr_D0:	dc.b 2, $50, $52, $80, $58	
-SME_Z3Iyr_D5:	dc.b 4, $50, $52, $50, $61, $50, $67, $10, $6D	
-SME_Z3Iyr_DE:	dc.b 2, $50, $52, $B0, $6F	
-SME_Z3Iyr_E3:	dc.b 3, $70, $30, $30, $7B, $50, $7F	
-SME_Z3Iyr_EA:	dc.b 5, $50, $8B, $20, $91, $50, $94, 0, $9A, $53, $1A	
-SME_Z3Iyr_F5:	dc.b 6, $50, $85, $10, $9B, $30, $9D, $50, $A1, $30, $A7, 0, $AB	
-SME_Z3Iyr_102:	dc.b 4, $50, $AC, $10, $B2, $70, $B4, $20, $BC	
-SME_Z3Iyr_10B:	dc.b 5, $30, $C5, $70, $C9, $20, $D1, $10, $D4, $53, $20	
-SME_Z3Iyr_116:	dc.b 4, $50, $BF, $10, $D6, $70, $D8, $20, $E0	
-SME_Z3Iyr_11F:	dc.b 5, $50, $85, $10, $9B, 0, $91, $70, $E3, $20, $EB	
-SME_Z3Iyr_12A:	dc.b 4, $70, $EE, $50, $F6, $10, $FC, $50, $FE	
-SME_Z3Iyr_133:	dc.b 2, $70, $EE, $B1, 4	
-SME_Z3Iyr_138:	dc.b 2, $51, $10, $81, $16	
-SME_Z3Iyr_13D:	dc.b 4, $51, $10, $51, $1F, $11, $25, $51, $27	
-SME_Z3Iyr_146:	dc.b 2, $51, $10, $B1, $2D	
-SME_Z3Iyr_14B:	dc.b 3, $70, $EE, 1, 4, $81, $39	
-SME_Z3Iyr_152:	dc.b 6, $11, $48, $11, $4A, $81, $4C, 1, $55, 1, $56, $53, $92	
-SME_Z3Iyr_15F:	dc.b 6, $51, $42, $21, $57, $11, $5A, $11, $5C, $81, $5E, 1, $55	
-SME_Z3Iyr_16C:	dc.b 4, $51, $67, $11, $6D, $81, $6F, $11, $78	
-SME_Z3Iyr_175:	dc.b 5, $21, $80, $11, $83, $71, $85, $21, $8D, $53, $9D	
-SME_Z3Iyr_180:	dc.b 4, $51, $7A, $11, $90, $81, $92, $11, $9B	
-SME_Z3Iyr_189:	dc.b 5, $51, $42, $81, $9D, $11, $5C, $11, $A6, 3, $A5	
-SME_Z3Iyr_194:	dc.b 2, $51, $A8, $B1, $AE	
-SME_Z3Iyr_199:	dc.b 2, $50, $52, $B1, $BA	
-SME_Z3Iyr_19E:	dc.b 2, $51, $A8, $B1, $C6	
-SME_Z3Iyr_1A3:	dc.b 2, $50, $52, $B1, $D2	
-SME_Z3Iyr_1A8:	dc.b 4, $51, $DE, $11, $E4, $B1, $E6, 1, $F2	
-SME_Z3Iyr_1B1:	dc.b 4, $51, $F3, $11, $F9, $B1, $FB, 3, $26	
-SME_Z3Iyr_1BA:	dc.b 6, $12, 7, $B2, 9, $13, $98, 3, $9A, $13, $9B, $13, $CD	
-SME_Z3Iyr_1C7:	dc.b 6, $B2, $15, $13, $27, $13, $A3, $13, $C9, $13, $CB, 3, $CF	
-SME_Z3Iyr_1D4:	dc.b 2, $52, $21, $B2, $27	
-SME_Z3Iyr_1D9:	dc.b 2, $51, $10, $B2, $33	
-SME_Z3Iyr_1DE:	dc.b 2, $52, $21, $B2, $3F	
-SME_Z3Iyr_1E3:	dc.b 2, $51, $10, $B2, $4B	
-SME_Z3Iyr_1E8:	dc.b 4, $52, $57, $12, $5D, $B2, $5F, 2, $6B	
-SME_Z3Iyr_1F1:	dc.b 3, $72, $6C, $B2, $74, 3, $DF	
-SME_Z3Iyr_1F8:	dc.b 4, $12, $80, $B2, $82, 2, $6B, $53, $D9	
-SME_Z3Iyr_201:	dc.b 3, $B2, $8E, 3, $D0, $73, $D1	
-SME_Z3Iyr_208:	dc.b 2, $34, $57, $B4, $4B	
-SME_Z3Iyr_20D:	dc.b 4, $34, $6A, $74, $5B, $34, $63, $24, $67	
-SME_Z3Iyr_216:	dc.b 3, $34, $7D, $B4, $6E, $24, $7A	
-SME_Z3Iyr_21D:	dc.b 2, $52, $9A, $B2, $A0	
-SME_Z3Iyr_222:	dc.b 4, $52, $AC, $72, $B2, $12, $BA, 2, $BC	
-SME_Z3Iyr_22B:	dc.b 4, $12, $BD, $72, $BF, $22, $C7, 2, $CA	
-SME_Z3Iyr_234:	dc.b 3, $22, $CB, $22, $CE, $F2, $D1	
-SME_Z3Iyr_23B:	dc.b 3, $B2, $E1, $72, $ED, 0, $6F	
-SME_Z3Iyr_242:	dc.b 3, $72, $F5, $32, $FD, $23, 1	
-SME_Z3Iyr_249:	dc.b 1, $83, 4	
-SME_Z3Iyr_24C:	dc.b 3, $73, $D, 3, $15, $33, $16	
-SME_Z3Iyr_253:	dc.b 3, $B3, $29, $13, $35, 3, $37	
-SME_Z3Iyr_25A:	dc.b 4, $B3, $38, $33, $44, 3, $48, 3, $49	
-SME_Z3Iyr_263:	dc.b 4, $B3, $4A, $33, $56, 3, $5A, 3, $5B	
-SME_Z3Iyr_26C:	dc.b 2, $83, $5C, $73, $65	
-SME_Z3Iyr_271:	dc.b 3, $83, $6D, $23, $76, $13, $79	
-SME_Z3Iyr_278:	dc.b 2, $83, $7B, $73, $84	
-SME_Z3Iyr_27D:	dc.b 3, $83, $6D, $23, $8C, $13, $8F	
-SME_Z3Iyr_284:	dc.b 5, $73, $A6, $13, $AE, $53, $B0, $33, $B6, 3, $BA	
-SME_Z3Iyr_28F:	dc.b 5, $73, $BB, $13, $AE, $53, $C3, $33, $B6, 3, $BA	
-SME_Z3Iyr_29A:	dc.b 3, $72, $F5, $32, $FD, $23, 1	
-SME_Z3Iyr_2A1:	dc.b 3, $73, $D, 3, $15, $33, $16	
-SME_Z3Iyr_2A8:	dc.b 3, $B3, $E0, $13, $EC, $33, $EE	
-SME_Z3Iyr_2AF:	dc.b 2, $F4, 4, $24, $14	
-SME_Z3Iyr_2B4:	dc.b 3, $53, $F2, $B3, $F8, $10, $6B	
-SME_Z3Iyr_2BB:	dc.b 4, $F4, $17, $14, $27, 4, $29, $14, $2A	
-SME_Z3Iyr_2C4:	dc.b 8, 4, $3E, $24, $39, $14, $3C, $14, $2C, $24, $2E, $34, $31, $24, $36, 4, $35	
-SME_Z3Iyr_2D5:	dc.b 1, $B4, $3F	
+SME_nPq2R:	
+		dc.w SME_nPq2R_94-SME_nPq2R, SME_nPq2R_95-SME_nPq2R	
+		dc.w SME_nPq2R_9E-SME_nPq2R, SME_nPq2R_AD-SME_nPq2R	
+		dc.w SME_nPq2R_B4-SME_nPq2R, SME_nPq2R_BB-SME_nPq2R	
+		dc.w SME_nPq2R_C2-SME_nPq2R, SME_nPq2R_CB-SME_nPq2R	
+		dc.w SME_nPq2R_D0-SME_nPq2R, SME_nPq2R_D5-SME_nPq2R	
+		dc.w SME_nPq2R_DE-SME_nPq2R, SME_nPq2R_E3-SME_nPq2R	
+		dc.w SME_nPq2R_EA-SME_nPq2R, SME_nPq2R_F5-SME_nPq2R	
+		dc.w SME_nPq2R_102-SME_nPq2R, SME_nPq2R_10B-SME_nPq2R	
+		dc.w SME_nPq2R_116-SME_nPq2R, SME_nPq2R_11F-SME_nPq2R	
+		dc.w SME_nPq2R_12A-SME_nPq2R, SME_nPq2R_133-SME_nPq2R	
+		dc.w SME_nPq2R_138-SME_nPq2R, SME_nPq2R_13D-SME_nPq2R	
+		dc.w SME_nPq2R_146-SME_nPq2R, SME_nPq2R_14B-SME_nPq2R	
+		dc.w SME_nPq2R_152-SME_nPq2R, SME_nPq2R_15F-SME_nPq2R	
+		dc.w SME_nPq2R_16C-SME_nPq2R, SME_nPq2R_175-SME_nPq2R	
+		dc.w SME_nPq2R_180-SME_nPq2R, SME_nPq2R_189-SME_nPq2R	
+		dc.w SME_nPq2R_194-SME_nPq2R, SME_nPq2R_199-SME_nPq2R	
+		dc.w SME_nPq2R_19E-SME_nPq2R, SME_nPq2R_1A3-SME_nPq2R	
+		dc.w SME_nPq2R_1A8-SME_nPq2R, SME_nPq2R_1B1-SME_nPq2R	
+		dc.w SME_nPq2R_1BA-SME_nPq2R, SME_nPq2R_1C7-SME_nPq2R	
+		dc.w SME_nPq2R_1D4-SME_nPq2R, SME_nPq2R_1D9-SME_nPq2R	
+		dc.w SME_nPq2R_1DE-SME_nPq2R, SME_nPq2R_1E3-SME_nPq2R	
+		dc.w SME_nPq2R_1E8-SME_nPq2R, SME_nPq2R_1F1-SME_nPq2R	
+		dc.w SME_nPq2R_1F8-SME_nPq2R, SME_nPq2R_201-SME_nPq2R	
+		dc.w SME_nPq2R_208-SME_nPq2R, SME_nPq2R_20D-SME_nPq2R	
+		dc.w SME_nPq2R_216-SME_nPq2R, SME_nPq2R_21D-SME_nPq2R	
+		dc.w SME_nPq2R_222-SME_nPq2R, SME_nPq2R_22B-SME_nPq2R	
+		dc.w SME_nPq2R_234-SME_nPq2R, SME_nPq2R_23B-SME_nPq2R	
+		dc.w SME_nPq2R_242-SME_nPq2R, SME_nPq2R_249-SME_nPq2R	
+		dc.w SME_nPq2R_24C-SME_nPq2R, SME_nPq2R_253-SME_nPq2R	
+		dc.w SME_nPq2R_25A-SME_nPq2R, SME_nPq2R_263-SME_nPq2R	
+		dc.w SME_nPq2R_26C-SME_nPq2R, SME_nPq2R_271-SME_nPq2R	
+		dc.w SME_nPq2R_278-SME_nPq2R, SME_nPq2R_27D-SME_nPq2R	
+		dc.w SME_nPq2R_284-SME_nPq2R, SME_nPq2R_28F-SME_nPq2R	
+		dc.w SME_nPq2R_29A-SME_nPq2R, SME_nPq2R_2A1-SME_nPq2R	
+		dc.w SME_nPq2R_2A8-SME_nPq2R, SME_nPq2R_2AF-SME_nPq2R	
+		dc.w SME_nPq2R_2B4-SME_nPq2R, SME_nPq2R_2BB-SME_nPq2R	
+		dc.w SME_nPq2R_2C4-SME_nPq2R, SME_nPq2R_2D5-SME_nPq2R	
+SME_nPq2R_94:	dc.b 0	
+SME_nPq2R_95:	dc.b 4, $20, 0, $20, 9, $20, $C, $50, 3	
+SME_nPq2R_9E:	dc.b 7, $20, $1B, $50, $F, 0, $1A, 3, $91, $10, $15, 0, $18, 4, $81	
+SME_nPq2R_AD:	dc.b 3, $50, $1E, $50, $15, $20, $1B	
+SME_nPq2R_B4:	dc.b 3, $50, $1E, $50, $15, $20, $24	
+SME_nPq2R_BB:	dc.b 3, $80, $27, $20, 9, $20, $C	
+SME_nPq2R_C2:	dc.b 4, $70, $30, $50, $38, $50, $3E, $10, $44	
+SME_nPq2R_CB:	dc.b 2, $70, $30, $B0, $46	
+SME_nPq2R_D0:	dc.b 2, $50, $52, $80, $58	
+SME_nPq2R_D5:	dc.b 4, $50, $52, $50, $61, $50, $67, $10, $6D	
+SME_nPq2R_DE:	dc.b 2, $50, $52, $B0, $6F	
+SME_nPq2R_E3:	dc.b 3, $70, $30, $30, $7B, $50, $7F	
+SME_nPq2R_EA:	dc.b 5, $50, $8B, $20, $91, $50, $94, 0, $9A, $53, $1A	
+SME_nPq2R_F5:	dc.b 6, $50, $85, $10, $9B, $30, $9D, $50, $A1, $30, $A7, 0, $AB	
+SME_nPq2R_102:	dc.b 4, $50, $AC, $10, $B2, $70, $B4, $20, $BC	
+SME_nPq2R_10B:	dc.b 5, $30, $C5, $70, $C9, $20, $D1, $10, $D4, $53, $20	
+SME_nPq2R_116:	dc.b 4, $50, $BF, $10, $D6, $70, $D8, $20, $E0	
+SME_nPq2R_11F:	dc.b 5, $50, $85, $10, $9B, 0, $91, $70, $E3, $20, $EB	
+SME_nPq2R_12A:	dc.b 4, $70, $EE, $50, $F6, $10, $FC, $50, $FE	
+SME_nPq2R_133:	dc.b 2, $70, $EE, $B1, 4	
+SME_nPq2R_138:	dc.b 2, $51, $10, $81, $16	
+SME_nPq2R_13D:	dc.b 4, $51, $10, $51, $1F, $11, $25, $51, $27	
+SME_nPq2R_146:	dc.b 2, $51, $10, $B1, $2D	
+SME_nPq2R_14B:	dc.b 3, $70, $EE, 1, 4, $81, $39	
+SME_nPq2R_152:	dc.b 6, $11, $48, $11, $4A, $81, $4C, 1, $55, 1, $56, $53, $92	
+SME_nPq2R_15F:	dc.b 6, $51, $42, $21, $57, $11, $5A, $11, $5C, $81, $5E, 1, $55	
+SME_nPq2R_16C:	dc.b 4, $51, $67, $11, $6D, $81, $6F, $11, $78	
+SME_nPq2R_175:	dc.b 5, $21, $80, $11, $83, $71, $85, $21, $8D, $53, $9D	
+SME_nPq2R_180:	dc.b 4, $51, $7A, $11, $90, $81, $92, $11, $9B	
+SME_nPq2R_189:	dc.b 5, $51, $42, $81, $9D, $11, $5C, $11, $A6, 3, $A5	
+SME_nPq2R_194:	dc.b 2, $51, $A8, $B1, $AE	
+SME_nPq2R_199:	dc.b 2, $50, $52, $B1, $BA	
+SME_nPq2R_19E:	dc.b 2, $51, $A8, $B1, $C6	
+SME_nPq2R_1A3:	dc.b 2, $50, $52, $B1, $D2	
+SME_nPq2R_1A8:	dc.b 4, $51, $DE, $11, $E4, $B1, $E6, 1, $F2	
+SME_nPq2R_1B1:	dc.b 4, $51, $F3, $11, $F9, $B1, $FB, 3, $26	
+SME_nPq2R_1BA:	dc.b 6, $12, 7, $B2, 9, $13, $98, 3, $9A, $13, $9B, $13, $CD	
+SME_nPq2R_1C7:	dc.b 6, $B2, $15, $13, $27, $13, $A3, $13, $C9, $13, $CB, 3, $CF	
+SME_nPq2R_1D4:	dc.b 2, $52, $21, $B2, $27	
+SME_nPq2R_1D9:	dc.b 2, $51, $10, $B2, $33	
+SME_nPq2R_1DE:	dc.b 2, $52, $21, $B2, $3F	
+SME_nPq2R_1E3:	dc.b 2, $51, $10, $B2, $4B	
+SME_nPq2R_1E8:	dc.b 4, $52, $57, $12, $5D, $B2, $5F, 2, $6B	
+SME_nPq2R_1F1:	dc.b 3, $72, $6C, $B2, $74, 3, $DF	
+SME_nPq2R_1F8:	dc.b 4, $12, $80, $B2, $82, 2, $6B, $53, $D9	
+SME_nPq2R_201:	dc.b 3, $B2, $8E, 3, $D0, $73, $D1	
+SME_nPq2R_208:	dc.b 2, $34, $57, $B4, $4B	
+SME_nPq2R_20D:	dc.b 4, $34, $6A, $74, $5B, $34, $63, $24, $67	
+SME_nPq2R_216:	dc.b 3, $34, $7D, $B4, $6E, $24, $7A	
+SME_nPq2R_21D:	dc.b 2, $52, $9A, $B2, $A0	
+SME_nPq2R_222:	dc.b 4, $52, $AC, $72, $B2, $12, $BA, 2, $BC	
+SME_nPq2R_22B:	dc.b 4, $12, $BD, $72, $BF, $22, $C7, 2, $CA	
+SME_nPq2R_234:	dc.b 3, $22, $CB, $22, $CE, $F2, $D1	
+SME_nPq2R_23B:	dc.b 3, $B2, $E1, $72, $ED, 0, $6F	
+SME_nPq2R_242:	dc.b 3, $72, $F5, $32, $FD, $23, 1	
+SME_nPq2R_249:	dc.b 1, $83, 4	
+SME_nPq2R_24C:	dc.b 3, $73, $D, 3, $15, $33, $16	
+SME_nPq2R_253:	dc.b 3, $B3, $29, $13, $35, 3, $37	
+SME_nPq2R_25A:	dc.b 4, $B3, $38, $33, $44, 3, $48, 3, $49	
+SME_nPq2R_263:	dc.b 4, $B3, $4A, $33, $56, 3, $5A, 3, $5B	
+SME_nPq2R_26C:	dc.b 2, $83, $5C, $73, $65	
+SME_nPq2R_271:	dc.b 3, $83, $6D, $23, $76, $13, $79	
+SME_nPq2R_278:	dc.b 2, $83, $7B, $73, $84	
+SME_nPq2R_27D:	dc.b 3, $83, $6D, $23, $8C, $13, $8F	
+SME_nPq2R_284:	dc.b 5, $73, $A6, $13, $AE, $53, $B0, $33, $B6, 3, $BA	
+SME_nPq2R_28F:	dc.b 5, $73, $BB, $13, $AE, $53, $C3, $33, $B6, 3, $BA	
+SME_nPq2R_29A:	dc.b 3, $72, $F5, $32, $FD, $23, 1	
+SME_nPq2R_2A1:	dc.b 3, $73, $D, 3, $15, $33, $16	
+SME_nPq2R_2A8:	dc.b 3, $B3, $E0, $13, $EC, $33, $EE	
+SME_nPq2R_2AF:	dc.b 2, $F4, 4, $24, $14	
+SME_nPq2R_2B4:	dc.b 3, $53, $F2, $B3, $F8, $10, $6B	
+SME_nPq2R_2BB:	dc.b 4, $F4, $17, $14, $27, 4, $29, $14, $2A	
+SME_nPq2R_2C4:	dc.b 8, 4, $3E, $24, $39, $14, $3C, $14, $2C, $24, $2E, $34, $31, $24, $36, 4, $35	
+SME_nPq2R_2D5:	dc.b 1, $B4, $3F	
 		even
