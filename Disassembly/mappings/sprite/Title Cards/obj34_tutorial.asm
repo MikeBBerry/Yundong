4b6be1e2fb4c8bbe2281c98ae5019768a54e54d1
Ralakimus <devon.artmeiercls@gmail.com>
2016-04-10 20:14:17 -0400

    Changed "Tutorial" to "It's a Tutorial"

--- Disassembly/mappings/sprite/Title Cards/obj34_tutorial.asm
+++ Disassembly/mappings/sprite/Title Cards/obj34_tutorial.asm
@@ -2,18 +2,23 @@
 ; Sprite mappings - output from SonMapEd - Sonic 1 format
 ; --------------------------------------------------------------------------------
 
-SME_RxGWg:	
-		dc.w SME_RxGWg_4-SME_RxGWg, SME_RxGWg_2D-SME_RxGWg	
-SME_RxGWg_4:	dc.b 8	
-		dc.b $FB, 5, 0, $1A, $D4	
-		dc.b $FB, 5, 0, $1E, $E4	
-		dc.b $FB, 5, 0, $1A, $F4	
-		dc.b $FB, 5, 0, $12, 4	
-		dc.b $FB, 5, 0, $16, $14	
-		dc.b $FB, 1, 0, 8, $24	
-		dc.b $FB, 5, 0, 0, $2C	
-		dc.b $FB, 5, 0, $A, $3C	
-SME_RxGWg_2D:	dc.b 6	
+SME_HbpHa:	
+		dc.w SME_HbpHa_4-SME_HbpHa, SME_HbpHa_46-SME_HbpHa	
+SME_HbpHa_4:	dc.b $D	
+		dc.b $FB, 5, 0, $1A, $D0	
+		dc.b $FB, 5, 0, $1E, $E0	
+		dc.b $FB, 5, 0, $1A, $F0	
+		dc.b $FB, 5, 0, $12, 0	
+		dc.b $FB, 5, 0, $16, $10	
+		dc.b $FB, 1, 0, 8, $20	
+		dc.b $FB, 5, 0, 0, $28	
+		dc.b $FB, 5, 0, $A, $38	
+		dc.b $EA, 5, 0, $2A, 4	
+		dc.b $EA, 0, 0, $2E, $FC	
+		dc.b $EA, 5, 0, $1A, $EC	
+		dc.b $EA, 1, 0, 8, $E4	
+		dc.b $EA, 5, 0, 0, $24	
+SME_HbpHa_46:	dc.b 6	
 		dc.b $F8, 5, 0, $12, $CC	
 		dc.b $F8, 5, 0, 4, $DC	
 		dc.b $F8, 5, 0, $E, $EC	
